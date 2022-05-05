07a70df9bd4f8c83fa277efccf6cffdb93c7d372
andriipalii <andrii.palii@idealscorp.com>
2022-05-05 15:11:58 +0200

    - added demo tests with different approaches

---
+++ test/org/livecodingtask/classic_page_object/SearchResultsPage.java
@@ -0,0 +1,15 @@
+package org.livecodingtask.classic_page_object;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchResultsPage {
+  @FindBy(css = ".results .result")
+  private List<WebElement> results;
+
+  public List<WebElement> getResults() {
+    return results;
+  }
+}
